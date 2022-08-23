f1ae6569762fb38d0ff0c8dcd1f32c63c27c4897
Mohammad Ikmal <111510512+MalSpielplatz@users.noreply.github.com>
2022-08-23 02:54:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("---------- Program Ganjil Genap ----------")
+
+print ("Masukkan Angka untuk mengetahui apakah angka itu ganjil atau genap ! ")
+angka = int(input("angka = "))
+
+if angka % 2 == 0:
+  print ("Genap")
+else:
+  print ("Ganjil")
